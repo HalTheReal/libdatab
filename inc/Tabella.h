d6f5a0a83a1256e6e0c2b166ffad525cca26c1e2
Enrico Chiarelli <therealfrenes@gmail.com>
2017-08-22 17:20:34 +0200

    Reimplementata classe Tabella
    inc/Tabella.h src/Tabella.cpp:
      - Classe tabella semplificata
      - Tutti i metodi per la gestione dei file fanno ora parte della
        classe FileReader
      - Mantanuta compatibilità con implementazione precedente per i
        metodi più importanti
      - Rimossa cache integrata

--- inc/Tabella.h
+++ inc/Tabella.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <limits>
+#include <FileReader.h>
 
 #include <tools.h>
 
@@ -14,35 +14,21 @@ class Tabella {
 
   private:
 
-    const static int cEnt = 10;               // Cache entries
-    int cacheIdx[cEnt];                       // Cache mapping
-    int nRighe;                               // Righe nel file
     char sep;                                 // Separatore dei campi tabella, ad es. ' '
-    int currentLine;
-    std::string fileName;
-    std::ifstream fp;
-    std::vector <std::string> cache[cEnt];    // Cache di stringhe
+    int totalFields;
+    FileReader fileR;
 
-    bool skipTo(int n);
-    bool openFile();
-    bool closeFile();
-    bool contaRighe();
-
-    void cacheInvalidation();
-    bool cacheFetch(int line);
-    int readCache(int line, int field, std::string &str);
-    int readLine(std::string &str, int n = 0);
+    std::string readLine(int riga);
+    int countFields();
 
   protected:
 
   public:
     Tabella(std::string nomeFile, char separator);
     Tabella(const char * nomeFile, char separator);
-    Tabella(const Tabella & src);
-    ~Tabella();
-    Tabella& operator = (const Tabella & src);
     int getLineNum();
     int getLine(int riga, std::string& str);
+    std::string getField(int riga, int field);
     int getField(int riga, int field, std::string& str);
     double readDouble(int riga, int field);
     std::string getTabName();
--- src/Tabella.cpp
+++ src/Tabella.cpp
@@ -1,204 +1,53 @@
 #include "Tabella.h"
 
-// Il costruttore necessita di un nome del file e di un carattere
-// separatore
-Tabella::Tabella(std::string nomeFile, char separator) {
-  currentLine = -1;
-  nRighe = -1;
-  fileName.assign(nomeFile);
-  sep = separator;
-  for (int i = 0; i < cEnt; i++) {
-    cacheIdx[i] = -1;
-  }
+Tabella::Tabella(std::string nomeFile, char separator)
+  : sep(separator)
+  , totalFields(0)
+  , fileR(nomeFile)
+{
+  totalFields = countFields();
 }
 
-// Costruttore equivalente al primo
-Tabella::Tabella(const char * nomeFile, char separator) {
-  currentLine = -1;
-  nRighe = -1;
-  fileName.assign(std::string(nomeFile));
-  sep = separator;
-  for (int i = 0; i < cEnt; i++) {
-    cacheIdx[i] = -1;
-  }
+Tabella::Tabella(const char * nomeFile, char separator)
+  : sep(separator)
+  , totalFields(0)
+  , fileR(nomeFile)
+{
+  totalFields = countFields();
 }
 
-Tabella& Tabella::operator = (const Tabella & src) {
-  if (this == &src) {
-    return *this;
-  }
-  for (int i = 0; i < cEnt; i++) {
-    cacheIdx[i] = src.cacheIdx[i];
-    cache[i] = src.cache[i];
-  }
-  nRighe = src.nRighe;
-  sep = src.sep;
-  fileName = src.fileName;
-  currentLine = -1;
-  return *this;
+std::string Tabella::readLine(int riga) {
+  return fileR.readLine(riga);
 }
 
-Tabella::Tabella(const Tabella & src) {
-  for (int i = 0; i < cEnt; i++) {
-    cacheIdx[i] = src.cacheIdx[i];
-    cache[i] = src.cache[i];
+std::string Tabella::getField(int line, int field) {
+  auto toks = tls::split(readLine(line), sep);
+  if (field < 0) {
+    return toks[totalFields + field];
   }
-  nRighe = src.nRighe;
-  sep = src.sep;
-  fileName = src.fileName;
-  currentLine = -1;
+  return toks[field - 1];
 }
-
-Tabella::~Tabella() {
-  closeFile();
-}
-
-bool Tabella::openFile() {
-  if (!fp.is_open()) {
-    fp.open(fileName.c_str());
-    currentLine = 0;
-    if (!fp) {
-      fprintf(stderr, "Impossibile aprire il file:\n%s\n", this->fileName.c_str());
-      return false;
-    }
-  }
-  return true;
-}
-
-bool Tabella::closeFile() {
-  fp.close();
-  fp.clear();
-  currentLine = -1;
-  return true;
-}
-
-bool Tabella::contaRighe() {
-  if (this->nRighe >= 0) {
-    return true;
-  }
-  if (!openFile()) {
-    return false;
-  }
-
-  int righeContate = -1;
-  while (fp.ignore(std::numeric_limits<std::streamsize>::max(), fp.widen('\n'))) {
-    righeContate++;
-  }
-
-  closeFile();
-
-  this->nRighe = righeContate;
-  return true;
-}
-
-bool Tabella::skipTo(int n) {
-  if (n == 0 || currentLine == n - 1) {    // 0 sta per "prossima riga"
-    currentLine++;
-    return true;
-  }
-  if (n < currentLine) {
-    closeFile();
-    if (!openFile()) {
-      return false;
-    }
-  }
-  while (currentLine < n - 1 && fp.ignore(std::numeric_limits<std::streamsize>::max(), fp.widen('\n'))) {
-    currentLine++;
-  }
-  if (currentLine != n - 1) {
-    return false;
-  }
-  currentLine = n;
-  return true; 
-}
-
-int Tabella::readLine(std::string &str, int n) {
-  if (!openFile()) {
-    return -1;
-  }
-  if (!skipTo(n)) {
-    closeFile();
-    return -1;
-  }
-  return (std::getline(fp, str) ? 1 : 0);
-}
-
-//int Tabella::getLine(int line, std::string& str) {
-//  std::vector <std::string> tokens;
-//  if (cacheFetch(line, tokens) == 0) {
-//    str = "";
-//    for (std::vector <std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it) {
-//      str += *it;
-//      if (it != tokens.end() - 1) {
-//        str += ' ';
-//      }
-//    }
-//    return 0;
-//  }
-//  return 1;
-//}
 
 int Tabella::getField(int line, int field, std::string& str) {
-  int cacheRet = readCache(line, field, str);
-  if (cacheRet == -3) {
-    std::cout << "Requested field is out of range!\n";
-  }
-  return cacheRet;
+  str = getField(line, field);
+  return 1;
+}
+
+int Tabella::countFields() {
+  auto toks = tls::split(readLine(2), sep);
+  return toks.size();
 }
 
 double Tabella::readDouble(int line, int field) {
-  std::string tmp;
-  getField(line, field, tmp);
+  std::string tmp = getField(line, field);
   return stod(tmp);
 }
 
-void Tabella::cacheInvalidation() {
-  for (int i = 0; i < this->cEnt; i++) {
-    this->cache[i].clear();
-  }
-}
-
-int Tabella::readCache(int line, int field, std::string &str) {
-  if (cacheFetch(line)) {
-    int lineSize = cache[(line -1) % cEnt].size();
-    if (field < 0) {
-      field += lineSize + 1;
-    }
-    if (field > 0 && field <= lineSize) {
-      str = cache[(line - 1) % cEnt][field - 1];
-      return 1;     // tutto ok
-    }
-    else {
-      return -3;    // out of range
-    }
-  }
-  return -1;        // impossibile leggere file
-}
-
-// Metodo privato di aggiornamento della cache
-bool Tabella::cacheFetch(int line) {
-  if (cacheIdx[(line - 1) % cEnt] != line) {      // Cache MISS
-    if(!contaRighe()) {                           // Conto righe totali
-      return false;
-    }
-    std::string riga;
-    if (!readLine(riga, line)) {
-      return false;
-    }
-    cache[(line - 1) % cEnt].clear();
-    cache[(line - 1) % cEnt] = tls::split(riga, this->sep);
-    cacheIdx[(line - 1) % cEnt] = line;
-  }
-  return true;
-}
-
 int Tabella::getLineNum() {
-  if(!contaRighe()) {
-    return -1;
-  }
-  return nRighe;
+  return fileR.getLineNum();
 }
 
 std::string Tabella::getTabName(){
-  return this->fileName;
+  std::string nome(fileR.getName());
+  return nome;
 }
