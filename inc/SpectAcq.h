ace6207be72e33f5ca7113d3cb769a5ff0b7d856
Enrico Chiarelli <therealfrenes@gmail.com>
2017-12-07 09:58:45 +0100

    Aggiunta classe SpectAcq
    SpectAcq è una classe derivata di Spectrum che rappresenta gli
    spettri di acquisizione.
    
    inc/SpectAcq.h:
      - Dichiarata interfaccia pubblica
      - Dichiarate helper functions
    
    src/SpectAcq.cpp:
      - Implementata interfaccia pubblica

---
+++ inc/SpectAcq.h
@@ -0,0 +1,36 @@
+#ifndef SPECTRUMACQ_H
+#define SPECTRUMACQ_H
+
+#include <Spectrum.h>
+#include <Data.h>
+
+namespace physics {
+
+  class SpectAcq : public Spectrum {
+
+    public:
+      SpectAcq();
+      SpectAcq(const std::vector <int> &hist, const Data &date, float t);
+      SpectAcq(const std::vector <float> &hist, const Data &date, float t);
+      SpectAcq(const std::vector <double> &hist, const Data &date, float t);
+
+      float getDT() const;
+      Data getDate() const;
+
+    private:
+      Data dataSp;
+      float dT;
+  };
+
+  SpectAcq readSPE(const char * nomeFile);
+  SpectAcq readSPT(const char * nomeFile);
+  SpectAcq readLST(const char * nomeFile);
+
+  double getCps(const SpectAcq &sp, double en1);
+  double getCps(const SpectAcq &sp, double en1, double en2);
+
+  void writeSPE(const SpectAcq &sp, const char * nomeFile);
+  void writeSPT(const SpectAcq &sp, const char * nomeFile);
+
+}
+#endif
---
+++ src/SpectAcq.cpp
@@ -0,0 +1,172 @@
+#include "SpectAcq.h"
+
+namespace physics {
+
+  SpectAcq::SpectAcq(const std::vector <int> &hist, const Data &date, float t)
+    : Spectrum(hist)
+    , dataSp(date)
+    , dT(t)
+  {}
+
+  SpectAcq::SpectAcq(const std::vector <float> &hist, const Data &date, float t)
+    : Spectrum(hist)
+    , dataSp(date)
+    , dT(t)
+  {}
+
+  SpectAcq::SpectAcq(const std::vector <double> &hist, const Data &date, float t)
+    : Spectrum(hist)
+    , dataSp(date)
+    , dT(t)
+  {}
+
+  float SpectAcq::getDT() const {
+    return dT;
+  }
+
+  Data SpectAcq::getDate() const {
+    return dataSp;
+  }
+}
+//  void Spectrum::writeSPT(const char * nomeFile) {
+//    using namespace std;
+//    ofstream outfile;
+//    outfile.open(nomeFile);
+//    if (outfile) {
+//      outfile << canali << " " << dT;
+//      if (mCal != 1) {
+//        outfile << " true ";
+//      }
+//      else {
+//        outfile << " false ";
+//      }
+//      outfile << qCal << " " << mCal << endl;
+//      outfile << "# S_TIME: 000 " << dataSpt.toString('-', ':', 'B') << endl;
+//      outfile << "# " << dataSpt.toString('-', ':', 'B') << "# DET # Spectrum.cpp" << endl;
+//      for (int i = 0; i < canali; ++i) {
+//        outfile << bin[i];
+//        if (i % 8 == 7) {
+//          outfile << endl;
+//        }
+//        else {
+//          outfile << " ";
+//        }
+//      }
+//    }
+//    outfile.close();
+//  }
+//
+//  void Spectrum::writeSPE(const char * nomeFile) {
+//    using namespace std;
+//    ofstream outfile;
+//    outfile.open(nomeFile);
+//    if (outfile) {
+//      outfile << "$SPEC_ID:\nSpectrum.cpp\n";
+//      outfile << "$DATE_MEA:\n" << dataSpt.toString('-', ':', 'B') << endl;
+//      outfile << "$MEAS_TIM:\n" << dT << " " << dT << endl;
+//      outfile << "$DATA:\n" << 0 << " " << canali - 1 << endl;
+//      for (int i = 0; i < canali; ++i) {
+//        outfile << bin[i] << endl;
+//      }
+//    }
+//    outfile.close();
+//  }
+//int Spectrum::readSPE(const char * nomeFile) {
+//  std::ifstream file(nomeFile);
+//  if (!file) {
+//    fprintf(stderr, "Impossibile aprire il file:\n%s\n", nomeFile);
+//    return 0;
+//  }
+//  bin.clear();
+//  std::string riga;
+//  while (std::getline(file,riga)) {
+//    if (riga[0] == '$') {
+//      std::vector <std::string> toks;
+//      if (riga.compare("$DATE_MEA:") == 0) {
+//        std::getline(file, riga);
+//        toks = tls::splitWhite(riga);
+//        dataSpt = Data(toks[0], toks[1], '-', ':', 'B');
+//      }
+//      else if (riga.compare("$MEAS_TIM:") == 0) {
+//        std::getline(file, riga);
+//        toks = tls::splitWhite(riga);
+//        dT = std::stof(toks[0]);
+//      }
+//      else if (riga.compare("$DATA:") == 0) {
+//        std::getline(file, riga);
+//        toks = tls::splitWhite(riga);
+//        canali = std::stod(toks[1]) + 1;
+//      }
+//      else {
+//        std::getline(file, riga);
+//      }
+//    }
+//    else {
+//      bin.push_back(stof(riga));
+//    }
+//  }
+//  file.close();
+//  return 1;
+//}
+//
+//int Spectrum::readSPT(const char * nomeFile) {
+//  std::ifstream file(nomeFile);
+//  if (!file) {
+//    fprintf(stderr, "Impossibile aprire il file:\n%s\n", nomeFile);
+//    return 0;
+//  }
+//  bin.clear();
+//  std::string riga;
+//  while (std::getline(file,riga)) {
+//    std::vector <std::string> toks = tls::splitWhite(riga);
+//    if (toks.size() == 5) {
+//      if (toks[1].compare("S_TIME:") == 0) {
+//        dataSpt = Data(toks[3], toks[4], '-', ':', 'B');
+//      }
+//      else {
+//        canali = std::stod(toks[0]);
+//        dT = std::stof(toks[1]);
+//        qCal = std::stod(toks[3]);
+//        mCal = std::stod(toks[4]);
+//      }
+//    }
+//    else if (toks.size() == 8) {
+//      for (int i = 0; i < 8; ++i) {
+//        bin.push_back(std::stof(toks[i]));
+//      }
+//    }
+//  }
+//  file.close();
+//  return 1;
+//}
+//
+//int Spectrum::readLST(const char * nomeFile) {
+//  std::ifstream file(nomeFile);
+//  if (!file) {
+//    fprintf(stderr, "Impossibile aprire il file:\n%s\n", nomeFile);
+//    return 0;
+//  }
+//  canali = 2048;
+//  bin.clear();
+//  bin.resize(canali, 0);
+//  std::string token;
+//  do {
+//    file >> token;
+//    if (token.compare("#StartTime:") == 0) {
+//      file >> token;
+//      std::vector <std::string> dateToks = tls::split(token, 'T');
+//      dataSpt = Data(dateToks[0], dateToks[1], '-', ':', 'B');
+//    }
+//  } while (token != "Gain");
+//
+//  long timeTok;
+//  double gainTok;
+//  int energyTok;
+//  while (file >> timeTok >> energyTok >> gainTok) {
+//    ++bin[energyTok];
+//  }
+//  dT = timeTok * 16E-9;
+//  file.close();
+//  return 1;
+//}
+//
