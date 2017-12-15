f6be3cb7054ff26117897b78ac652fab986aa0f0
Enrico Chiarelli <therealfrenes@gmail.com>
2017-12-15 15:17:29 +0100

    Implementazione interfaccia classe Time

--- inc/Time.h
+++ inc/Time.h
@@ -7,4 +7,30 @@
 #include <iostream>     // std::cout
 #include <math.h>       // fmod
 
+namespace Chrono {
+
+  class Time {
+    public:
+      Time();
+      Time(int secs);
+      Time(int hs, int ms, int ss);
+
+      int hour() const;
+      int min() const;
+      int sec() const;
+      
+      Time& addSeconds(int secs);
+
+    private:
+      int hours;
+      int minutes;
+      int seconds;
+  };
+
+  int toInt(const Time &tm);
+  std::string toString(const Time &tm);
+  Time strToTime(const std::string &str);
+
+}
+
 #endif
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,1 +1,60 @@
 #include "Time.h"
+
+namespace Chrono {
+
+  Time::Time()
+    : hours(0)
+    , minutes(0)
+    , seconds(0)
+  {}
+
+  // TODO Eccezione se l'orario non è valido
+  Time::Time(int hs, int ms, int ss)
+    : hours(hs)
+    , minutes(ms)
+    , seconds(ss)
+  {}
+
+  Time::Time(int secs)
+    : Time()
+  {
+    int usable = secs - ((secs / 86400) * 86400);
+    if (usable < 0) {
+      usable += 86400;
+    }
+    hours = usable / 3600;
+    usable -= hours * 3600;
+    minutes = usable / 60;
+    usable -= minutes * 60;
+    seconds = usable;
+  }
+
+  int Time::hour() const {
+    return hours;
+  }
+
+  int Time::min() const {
+    return minutes;
+  }
+
+  int Time::sec() const {
+    return seconds;
+  }
+
+  Time& Time::addSeconds(int secs) {
+    int base = toInt(*this);
+    Time add(base + secs);
+    hours = add.hour();
+    minutes = add.min();
+    seconds = add.sec();
+    return *this;
+  }
+
+  int toInt(const Time &tm) {
+    int ret = 0;
+    ret += tm.sec();
+    ret += tm.min() * 60;
+    ret += tm.hour() * 60 * 60;
+    return ret;
+  }
+}
