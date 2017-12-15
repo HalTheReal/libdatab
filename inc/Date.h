686c299b0032a59b6decd61a2673d19538c12ce7
Enrico Chiarelli <therealfrenes@gmail.com>
2017-12-15 16:37:13 +0100

    Aggiunti file per la classe Date

---
+++ inc/Date.h
@@ -0,0 +1,14 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdexcept>    // Eccezioni
+#include <iostream>     // std::cout
+#include <math.h>       // fmod
+
+namespace Chrono {
+}
+
+#endif
