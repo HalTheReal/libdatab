fec87aad6b08469b33c1af6730c75cf56077f107
Enrico Chiarelli <therealfrenes@gmail.com>
2017-12-17 13:33:57 +0100

    Dichiarata interfaccia base classe DateTime

--- inc/DateTime.h
+++ inc/DateTime.h
@@ -6,12 +6,28 @@
 #include <vector>
 #include <stdexcept>    // Eccezioni
 #include <iostream>     // std::cout
+#include <Time.h>
+#include <Date.h>
 
 namespace Chrono {
 
   class DateTime {
 
+    public:
+      DateTime();
+      DateTime(int dy, int mn, int yr, int hr, int mi, int sc);
+      DateTime(const Time &tm, const Date &dt);
+
+      DateTime& addDay(int toAdd);
+      DateTime& addSec(int toAdd);
+      
+    private:
+      Date dt;
+      Time tm;
+
   };
+
+  int toUnix(const DateTime &dtt);
 
 }
 
