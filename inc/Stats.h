9dcaa73b7f5dd604439c13e7cbf134bf09577442
Enrico Chiarelli <therealfrenes@gmail.com>
2018-12-18 14:53:56 +0100

    Rimosse versioni template con nodata
    Si può usare std::remove per togliere i nodata dai dati di input
    prima di applicare medie, somme ecc. Usare degli overload per questi
    template rende solo la libreria più difficile da mantenere.

--- inc/Stats.h
+++ inc/Stats.h
@@ -17,19 +17,6 @@ namespace Stats {
     }
 
   template <typename InputIterator>
-    typename std::iterator_traits<InputIterator>::value_type mean(InputIterator first, InputIterator last, int nodata) {
-      typename std::iterator_traits<InputIterator>::value_type tot = 0;
-      int count = 0;
-      for (; first != last; ++first) {
-        if (*first != nodata) {
-          tot += *first;
-          ++count;
-        }
-      }
-      return count == 0 ? nodata : tot / count;
-    }
-
-  template <typename InputIterator>
     typename std::iterator_traits<InputIterator>::value_type stDev(InputIterator first, InputIterator last) {
       auto media = mean(first, last);
       typename std::iterator_traits<InputIterator>::value_type tot = 0;
@@ -42,39 +29,12 @@ namespace Stats {
     }
 
   template <typename InputIterator>
-    typename std::iterator_traits<InputIterator>::value_type stDev(InputIterator first, InputIterator last, int nodata) {
-      auto media = mean(first, last, nodata);
-      typename std::iterator_traits<InputIterator>::value_type tot = 0;
-      int count = 0;
-      for (; first != last; ++first) {
-        if (*first != nodata) {
-          tot += pow(*first - media, 2);
-          ++count;
-        }
-      }
-      return count == 0 ? nodata : sqrt(tot / count);
-    }
-
-  template <typename InputIterator>
     typename std::iterator_traits<InputIterator>::value_type sum(InputIterator first, InputIterator last) {
       typename std::iterator_traits<InputIterator>::value_type tot = 0;
       for (; first != last; ++first) {
         tot += *first;
       }
       return tot;
-    }
-
-  template <typename InputIterator>
-    typename std::iterator_traits<InputIterator>::value_type sum(InputIterator first, InputIterator last, int nodata) {
-      typename std::iterator_traits<InputIterator>::value_type tot = 0;
-      int count = 0;
-      for (; first != last; ++first) {
-        if (*first != nodata) {
-          tot += *first;
-          ++count;
-        }
-      }
-      return count == 0 ? nodata : tot;
     }
 
   template <typename InputIterator>
