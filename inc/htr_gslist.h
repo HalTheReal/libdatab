749bfbaae82f8e2b960464b5c06941814361f9f8
Enrico Chiarelli <therealfrenes@gmail.com>
2019-07-29 16:40:18 +0200

    TimedEvent basato sui nanosecondi

--- inc/htr_gslist.h
+++ inc/htr_gslist.h
@@ -10,6 +10,7 @@
 #include <exception>
 #include <utility>      //  std::pair
 #include <algorithm>    //  std::sort
+#include <chrono>       //  std::duration
 #include <cmath>
 
 #include <htr_datetime.h>
@@ -21,64 +22,98 @@ namespace Spectrometry {
   class TimedEvent {
     public:
       TimedEvent();
-      TimedEvent(long long tstamp, unsigned energy);
-      long long time() const;
+      TimedEvent(int64_t tstamp, unsigned energy);
+
+      template <typename T>
+      TimedEvent(const T &tstamp, unsigned energy)
+        : evTime(std::chrono::duration_cast<std::chrono::nanoseconds> (tstamp))
+        , evEnergy(energy)
+      {}
+
+      std::chrono::nanoseconds time() const;
       unsigned energy() const;
-      TimedEvent& setTime(long long time);
+
+      template <typename T>
+      TimedEvent& setTime(const T &tstamp) {
+        evTime = std::chrono::duration_cast<std::chrono::nanoseconds> (tstamp);
+        return *this;
+      }
+
       TimedEvent& setEnergy(unsigned energy);
 
     private:
-      long long evTime;
+      std::chrono::nanoseconds evTime;
       unsigned evEnergy;
   };
 
-  void shift(TimedEvent &ev, long long offset);
+  template <typename T>
+  void shift(TimedEvent &evt, const T &offset) {
+    evt.setTime(evt.time() + std::chrono::duration_cast<std::chrono::nanoseconds> (offset));
+  }
+
   bool isBefore(const TimedEvent &ev1, const TimedEvent &ev2);
   bool isAfter(const TimedEvent &ev1, const TimedEvent &ev2);
   bool isSync(const TimedEvent &ev1, const TimedEvent &ev2);
-  bool isBefore(const TimedEvent &ev1, long long time);
-  bool isAfter(const TimedEvent &ev1, long long time);
-  bool isSync(const TimedEvent &ev1, long long time);
+
+  template <typename T>
+  bool isBefore(const TimedEvent &evt, const T &tstamp) {
+    return evt.time() < std::chrono::duration_cast<std::chrono::nanoseconds> (tstamp);
+  }
+
+  template <typename T>
+  bool isAfter(const TimedEvent &evt, const T &tstamp) {
+    return !isBefore(evt, tstamp);
+  }
+
+  template <typename T>
+  bool isSync(const TimedEvent &evt, const T &tstamp) {
+    return evt.time() == std::chrono::duration_cast<std::chrono::nanoseconds> (tstamp);
+  }
+
+  std::ostream& operator << (std::ostream &stream, const TimedEvent &evt);
+  std::istream& operator >> (std::istream &stream, TimedEvent &evt);
 
   using EventList = std::list<TimedEvent>;
 
-  bool isBeforeEvent(const TimedEvent &ev1, const TimedEvent &ev2);
-  bool isBeforeTime(const TimedEvent &ev1, long long time);
-  EventList timeCut(EventList &evl, long long from, long long to);
-  EventList timeCopy(const EventList &evl, long long from, long long to);
-  void timeShift(EventList &evl, long long offset);
-  void append(EventList &dest, EventList &toApp);
-  std::vector <int> toHistogram(const EventList &evl, std::size_t channels);
-
-  EventList readASCII(const char * filename);
-
+  void timeSort(EventList &evl);
+//
+//  bool isBeforeEvent(const TimedEvent &ev1, const TimedEvent &ev2);
+//  bool isBeforeTime(const TimedEvent &ev1, long long time);
+//  EventList timeCut(EventList &evl, long long from, long long to);
+//  EventList timeCopy(const EventList &evl, long long from, long long to);
+//  void timeShift(EventList &evl, long long offset);
+//  void append(EventList &dest, EventList &toApp);
+//  std::vector <int> toHistogram(const EventList &evl, std::size_t channels);
+//
+//  EventList readASCII(const char * filename);
+//
 //  class GSList {
 //
 //    public:
 //      GSList();
-//      GSList(const std::vector <Event> &events, const Epoch::DateTime &start);
+//      GSList(const EventList &evl, const Epoch::DateTime &start);
 //
-//      GSList copy(ll_int from, ll_int to) const;
+//      GSList copy(int fromSec, int toSec) const;
+//      GSList& erase(const GSList &gsl, int fromSec, int toSec);
 //      GSList& merge(const GSList &gsl);
 //
-//      std::vector <int> getEventHist() const;
+//      std::vector <int> getHistogram() const;
 //      Epoch::DateTime getDateTime() const;
 //      double getDT() const;
 //
 //      void writeGSL(const char * nomeFile) const;
 //
 //    private:
-//      EventList evtList;
+//      EventList eventList;    // 1E-9 [s], [keV]
 //      Epoch::DateTime dataGS;
+//      
+//      long tousec(int sec) const;
 //  };
 //
 //  Spectrometry::Spectrum toSpectrum();
 //  void writeSPE(const GSList &lst, const char * nomeFile);
 //  void writeSPT(const GSList &lst, const char * nomeFile);
 //  GSList readGSL(const char * nomeFile);
-//
-//  bool isBefore(Event evt, ll_int val);
-//  long tousec(long sec);
 }
 
 #endif
--- src/htr_gslist.cpp
+++ src/htr_gslist.cpp
@@ -6,12 +6,12 @@ TimedEvent::TimedEvent()
   , evEnergy(0)
 {}
 
-TimedEvent::TimedEvent(long long tstamp, unsigned energy)
+TimedEvent::TimedEvent(int64_t tstamp, unsigned energy)
   : evTime(tstamp)
   , evEnergy(energy)
 {}
 
-long long TimedEvent::time() const {
+std::chrono::nanoseconds TimedEvent::time() const {
   return evTime;
 }
 
@@ -19,18 +19,9 @@ unsigned TimedEvent::energy() const {
   return evEnergy;
 }
 
-TimedEvent& TimedEvent::setTime(long long time) {
-  evTime = time;
-  return *this;
-}
-
 TimedEvent& TimedEvent::setEnergy(unsigned energy) {
   evEnergy = energy;
   return *this;
-}
-
-void shift(TimedEvent &ev, long long offset) {
-  ev.setTime(ev.time() + offset);
 }
 
 bool isBefore(const TimedEvent &ev1, const TimedEvent &ev2) {
@@ -38,119 +29,96 @@ bool isBefore(const TimedEvent &ev1, const TimedEvent &ev2) {
 }
 
 bool isAfter(const TimedEvent &ev1, const TimedEvent &ev2) {
-  return !isBefore(ev1, ev2);
+  return ev1.time() > ev2.time();
 }
 
 bool isSync(const TimedEvent &ev1, const TimedEvent &ev2) {
   return ev1.time() == ev2.time();
 }
 
-bool isBefore(const TimedEvent &ev1, long long time) {
-  return ev1.time() < time;
-}
-
-bool isAfter(const TimedEvent &ev1, long long time) {
-  return !isBefore(ev1, time);
-}
-
-bool isSync(const TimedEvent &ev1, long long time) {
-  return ev1.time() == time;
-}
-
-bool isBeforeEvent(const TimedEvent &ev1, const TimedEvent &ev2) {
-  return isBefore(ev1, ev2);
-}
-
-bool isBeforeTime(const TimedEvent &ev1, long long time) {
-  return isBefore(ev1, time);
-}
-
 void timeSort(EventList &evl) {
-  evl.sort(isBeforeEvent);
+  evl.sort([](const TimedEvent &ev1, const TimedEvent &ev2) -> bool {
+    return ev1.time() < ev2.time();
+    });
 }
 
-EventList timeCut(EventList &evl, long long from, long long to) {
-  if(to < from) {
-    std::swap(from, to);
-  }
-  auto cutFrom = std::lower_bound(evl.begin(), evl.end(), from, isBeforeTime);
-  auto cutTo = std::lower_bound(evl.begin(), evl.end(), to, isBeforeTime);
-  EventList cut;
-  cut.splice(cut.begin(), evl, cutFrom, cutTo);
-  return cut;
-}
-
-EventList timeCopy(const EventList &evl, long long from, long long to) {
-  if(to < from) {
-    std::swap(from, to);
-  }
-  auto copyFrom = std::lower_bound(evl.begin(), evl.end(), from, isBeforeTime);
-  auto copyTo = std::lower_bound(evl.begin(), evl.end(), to, isBeforeTime);
-  return EventList(copyFrom, copyTo);
-}
-
-void timeShift(EventList &evl, long long offset) {
-  for (auto &event : evl) {
-    shift(event, offset);
-  }
-}
-
-void append(EventList &dest, EventList &toApp) {
-  long long end = dest.back().time();
-  timeShift(toApp, end);
-  dest.merge(toApp, isBeforeEvent);
-}
-
-std::vector <int> toHistogram(const EventList &evl, std::size_t channels) {
-  std::vector <int> hist(channels, 0);
-  for (auto &evt : evl) {
-    if (evt.energy() < channels) {
-      ++hist[evt.energy()];
-    }
-  }
-  return hist;
-}
-
-EventList readASCII(const char * filename);
-
-//GSList::GSList()
-//  : evtList()
-//  , dataGS()
-//{}
-//
-//GSList::GSList(const std::vector <TimedEvent> &events, const Epoch::DateTime &start)
-//  : evtList(events)
-//  , dataGS(start)
-//{}
-//
-//GSList GSList::copy(ll_int from, ll_int to) const {
+////EventList timeCut(EventList &evl, long long from, long long to) {
 //  if(to < from) {
 //    std::swap(from, to);
 //  }
-//  if(to <= 0) {
-//    return GSList();
-//  }
-//  auto newList = evtList.copy(from, to);
-//  Epoch::DateTime dtt = dataGS;
-//  if(from > 0) {
-//    dtt.addSec(from);
-//    for(auto &evt : copied) {
-//      evt.first -= tousec(from);
-//    }
-//  }
-//  return GSList(copied, dtt);
+//  auto cutFrom = std::lower_bound(evl.begin(), evl.end(), from, isBeforeTime);
+//  auto cutTo = std::lower_bound(evl.begin(), evl.end(), to, isBeforeTime);
+//  EventList cut;
+//  cut.splice(cut.begin(), evl, cutFrom, cutTo);
+//  return cut;
 //}
 //
-//long tousec(long sec) {
+//EventList timeCopy(const EventList &evl, long long from, long long to) {
+//  if(to < from) {
+//    std::swap(from, to);
+//  }
+//  auto copyFrom = std::lower_bound(evl.begin(), evl.end(), from, isBeforeTime);
+//  auto copyTo = std::lower_bound(evl.begin(), evl.end(), to, isBeforeTime);
+//  return EventList(copyFrom, copyTo);
+//}
+//
+//void timeShift(EventList &evl, long long offset) {
+//  for (auto &event : evl) {
+//    shift(event, offset);
+//  }
+//}
+//
+//void append(EventList &dest, EventList &toApp) {
+//  long long end = dest.back().time();
+//  timeShift(toApp, end);
+//  dest.merge(toApp, isBeforeEvent);
+//}
+//
+//std::vector <int> toHistogram(const EventList &evl, std::size_t channels) {
+//  std::vector <int> hist(channels, 0);
+//  for (auto &evt : evl) {
+//    if (evt.energy() < channels) {
+//      ++hist[evt.energy()];
+//    }
+//  }
+//  return hist;
+//}
+//
+//EventList readASCII(const char * filename);
+
+//GSList::GSList()
+//  : eventList()
+//  , dataGS()
+//{}
+//
+//GSList::GSList(const EventList &evl, const Epoch::DateTime &start)
+//  : eventList(evl)
+//  , dataGS(start)
+//{}
+//
+//GSList GSList::copy(int fromSec, int toSec) const {
+//  if(toSec < fromSec) {
+//    std::swap(fromSec, toSec);
+//  }
+//  if(toSec <= 0) {
+//    return GSList();
+//  }
+//  EventList newList = timeCopy(eventList, fromSec, toSec);
+//  Epoch::DateTime dtt = dataGS;
+//  if(fromSec > 0) {
+//    dtt.addSec(fromSec);
+//    timeShift(newList, - tousec(fromSec));
+//  }
+//  return GSList(newList, dtt);
+//}
+//
+//long GSList::tousec(int sec) const {
 //  return sec / 1E-9;
 //}
 //
-//std::vector <int> GSList::getEventHist() const {
-//  std::vector <int> bin(2048, 0);
-//  for (auto &evt : this->evtList) {
-//    ++bin[evt.second];
-//  }
-//  return bin;
+//
+//std::vector <int> GSList::getHistogram() const {
+//  return toHistogram(eventList, 2048);
 //}
 //
 //Epoch::DateTime GSList::getDateTime() const {
@@ -158,46 +126,30 @@ EventList readASCII(const char * filename);
 //}
 //
 //double GSList::getDT() const {
-//  return evtList.back().first * 1E-9;
+//  return eventList.back().time() * 1E-9;
 //}
 //
 //GSList& GSList::erase(long from, long to) {
-//  if(to < from) {
-//    std::swap(from, to);
-//  }
-//  if(to <= 0) {
-//    return *this;
-//  }
-//  auto eraseFrom = std::lower_bound(evtList.begin(), evtList.end(), tousec(from), isBefore);
-//  auto eraseTo = std::lower_bound(evtList.begin(), evtList.end(), tousec(to), isBefore);
-//  evtList.erase(eraseFrom, eraseTo);
-//  if(from <= 0) {
-//    dataGS.addSec(to);
-//    for(auto &evt : evtList) {
-//      evt.first -= tousec(to);
-//    }
-//  }
-//  return *this;
 //}
 //
 //GSList& GSList::merge(const GSList &gsl) {
 //  long offset = tousec(std::abs(Epoch::toUnix(dataGS) - Epoch::toUnix(gsl.dataGS)));
 //  if(dataGS > gsl.dataGS) {
-//    for(auto &evt : evtList) {
+//    for(auto &evt : eventList) {
 //      evt.first += offset;
 //    }
-//    for(auto evt : gsl.evtList) {
-//      evtList.push_back(evt);
+//    for(auto evt : gsl.eventList) {
+//      eventList.push_back(evt);
 //    }
 //    dataGS = gsl.dataGS;
 //  }
 //  else {
-//    for(auto evt : gsl.evtList) {
+//    for(auto evt : gsl.eventList) {
 //      evt.first += offset;
-//      evtList.push_back(evt);
+//      eventList.push_back(evt);
 //    }
 //  }
-//  std::sort(evtList.begin(), evtList.end());
+//  std::sort(eventList.begin(), eventList.end());
 //  return *this;
 //}
 //
@@ -205,21 +157,21 @@ EventList readASCII(const char * filename);
 //  std::ofstream outfile;
 //  outfile.open(nomeFile);
 //  if (outfile) {
-//    outfile << "#GammaStream 1.0 LIST\n";
+//    outfile << "#GammaStream 1.5 LIST\n";
 //    outfile << "#StartTime: ";
 //    outfile << dataGS.year() << '-' << dataGS.month() << '-';
 //    outfile << dataGS.day() << 'T' << Epoch::toTime(dataGS) << '\n';
 //    outfile << "#Fields: Time\tEnergy\tGain" << '\n';
-//    for (auto &evt : this->evtList) {
-//      outfile << std::right << std::setw(9) << evt.first / 16 << '\t';   // 1 ciclo clock = 16us
-//      outfile << std::setw(6) << evt.second << '\t';
+//    for (auto &evt : this->eventList) {
+//      outfile << std::right << std::setw(9) << evt.time() << '\t';
+//      outfile << std::setw(6) << evt.energy() << '\t';
 //      outfile << "1.000" << '\n';
 //    }
 //  }
 //}
 //
 //Spectrometry::Spectrum toSpectrum(const GSList &gsl) {
-//  return Spectrometry::Spectrum(gsl.getEventHist(), gsl.getDateTime(), gsl.getDT());
+//  return Spectrometry::Spectrum(gsl.getHistogram(), gsl.getDateTime(), gsl.getDT());
 //}
 //
 //void writeSPE(const GSList &lst, const char * nomeFile) {
@@ -259,9 +211,5 @@ EventList readASCII(const char * filename);
 //  file.close();
 //  return GSList(coppie, dataGS);
 //}
-//
-//bool isBefore(Event evt, ll_int val) {
-//  return (evt.first < val);
-//}
-//
+
 }
